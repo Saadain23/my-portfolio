ad6449a0b71ac3b209164acadcf92d18f442787f
Saadain23 <saadaink23@gmail.com>
2024-04-23 00:09:53 +0300

    first version of personal website

---
+++ commands.js
@@ -0,0 +1,103 @@
+var password = "saadain123";
+var linkedin = "https://www.linkedin.com/in/saadaink";
+var instagram = "https://www.instagram.com/khan.saadain/";
+var github = "https://github.com/Saadain23/";
+var resume = "https://drive.google.com/file/d/1sMsHOy0MoPMUrXRiYviguodGtIsqG2rQ/view?usp=sharing"
+var email = 'mailto:saadaink23@gmail.com';
+
+whois = [
+  "<br>",
+  "Hello, I'm Saadain Khan",
+  "A 20-year-old Data & Tech enthusiast with a strong academic foundation in the IB diploma program, ",
+  "which fostered my independence and passion for academic writing and research.",
+  "Currently pursuing a degree in Software & Systems Engineering, I'm excited to bridge the gap between",
+  "theory and practice. My professional journey includes a role as a Support Specialist at Free.fi .",
+  "Here, I honed my tech support related tasks and IAM skills, excelling in problem-solving.",
+  "Moreover, this role also consisted advanced querying and manipulation using SQL,automation scripts with python, node.js",
+  "Worked on analytics and BI tools such as Google's Bigquery and Lookerstudio ",
+  "Such an extent of exposure to Data made me passionate in the field of Data engineering.",
+  "",
+  "Recently, exploring Data Science, Data engineering and AI development (llm, langchain, etc.)",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  '<span class="heading">Type the command or click the respective url</span>',
+  "<br>",
+  '<span class="command">linkedin </span>       <a href="' + linkedin + '" target="_blank">linkedin/saadaink' + "</a>",
+  '<span class="command">instagram</span>       <a href="' + instagram + '" target="_blank">instagram/khan.saadain' + '</a>',
+  '<span class="command">github   </span>       <a href="' + github + '" target="_blank">github/Saadain23' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... some projects are offline,few on GitHub or confidential.",
+  "<br>"
+];
+
+skills = [
+  "<br>",
+  '<span class="skills">HTML, CSS, JS</span>         <span class="good-8">[########  ]</span>   ',
+  '<span class="skills">Python, SQL  </span>         <span class="good-9">[######### ]</span>   ',
+  '<span class="skills">Java, C      </span>         <span class="good-6">[######    ]</span>   ',
+  "<br>",
+  '<span class="skills">Software/ Data Eng </span>         <span class="good-8">[########  ]</span>   ',
+  "<br>",
+  "<span class=\"skills\">Find more by typing </span> <span class=\"command\">'linkedin'</span> <span class=\"skills\">or</span> <span class=\"command\">'resume'</span>",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="skills">Type any of the below commands and press Enter.</span>',
+  "<br>",
+  '<span class="heading">About me</span>',
+  "<br>",
+  '<span class="command">whois</span>          Who is Saadain?',
+  '<span class="command">social</span>         Displays social networks',
+  '<span class="command">skills</span>         Displays tech skills',
+  '<span class="command">resume</span>         View my resume',
+  '<span class="command">projects</span>       View projects',
+  '<span class="command">email</span>          Don\'t spam my email',
+  "<br>",
+  '<span class="heading">Utility commands</span>',
+  "<br>",
+  '<span class="command">help</span>           You should already know what this does',
+  '<span class="command">clear</span>          Clears the terminal',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">banner</span>         Displays the cool ASCII header',
+  "<br>",
+  '<span class="heading">Random</span>',
+  "<br>",
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">secret</span>         Find the password',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Saadain Khan, not a Corporation, still All rights reserved.</span>',
+  '<span class="banner">  /$$$$$$                           /$$            /$$                 /$$   /$$ /$$                          </span>',
+  '<span class="banner"> /$$__  $$                          | $$           |__/                | $$  /$$/| $$                          </span>',
+  '<span class="banner">|$$  \\__/  /$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$  /$$ /$$$$$$$        | $$ /$$/ | $$$$$$$   /$$$$$$  /$$$$$$$ </span>',
+  '<span class="banner">| $$$$$$  |____  $$ |____  $$ /$$__  $$ |____  $$| $$| $$__  $$       | $$$$$/  | $$__  $$ |____  $$| $$__  $$</span>',
+  '<span class="banner"> \\____  $$  /$$$$$$$  /$$$$$$$| $$  | $$  /$$$$$$$| $$| $$  \\ $$       | $$  $$  | $$  \\ $$  /$$$$$$$| $$  \\ $$</span>',
+  '<span class="banner"> /$$  \\ $$ /$$__  $$ /$$__  $$| $$  | $$ /$$__  $$| $$| $$  | $$       | $$\\  $$ | $$  | $$ /$$__  $$| $$  | $$</span>',
+  '<span class="banner">| $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$| $$  | $$       | $$ \\  $$| $$  | $$|  $$$$$$$| $$  | $$</span>',
+  '<span class="banner"> \\______/  \\_______/ \\_______/ \\_______/ \\_______/|__/|__/  |__/       |__/  \\__/|__/  |__/ \\_______/|__/  |__/ © 2003</span>',                                                                                                                                                                                                                                                                                                                                     
+  '<span class="color2">Welcome to my interactive terminal portfolio.</span>',
+  "<span class=\"color2\">For the list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
